2322609f2e874aa9d7bc3a7484f83f9c360cc78c
coleelam <elamc@purdue.edu>
2015-11-16 22:35:41 -0500

    fixed CircularBuffer tests, implemented them in Project4SampleTest

--- src/CircularBufferTest.java
+++ src/CircularBufferTest.java
@@ -20,35 +20,37 @@ public class CircularBufferTest {
 
     @Test
     public void test1() throws Exception {
-        Assert.assertEquals(buffer.getNewest(1)[0], "fourth");
+        Assert.assertEquals(buffer.getNewest(1)[0], "0003) fourth");
     }
     @Test
     public void test2() throws Exception {
-        String[] expected = {"second", "third", "fourth"};
+        String[] expected = {"0001) second", "0002) third", "0003) fourth"};
         Assert.assertArrayEquals(buffer.getNewest(3), expected);
     }
     @Test
     public void test3() throws Exception {
         buffer.put("fifth");
-        String[] expected = {"fourth", "fifth"};
+        String[] expected = {"0003) fourth", "0004) fifth"};
         Assert.assertArrayEquals(buffer.getNewest(2), expected);
     }
     @Test
     public void test4() throws Exception {
         CircularBuffer trash = new CircularBuffer(0);
-        Assert.assertArrayEquals(trash.getNewest(1), null);
+        String[] expected = null;
+        Assert.assertNull(trash.getNewest(1).toString().trim(), expected);
     }
     @Test
     public void test5() throws Exception {
         CircularBuffer trash = new CircularBuffer(1);
-        Assert.assertArrayEquals(trash.getNewest(1), null);
+        String[] expected = null;
+        Assert.assertNull(trash.getNewest(1).toString().trim(), expected);
     }
     @Test
     public void test6() throws Exception {
         CircularBuffer trash = new CircularBuffer(1);
         trash.put("bad");
         trash.put("good");
-        String[] expected = {"good"};
+        String[] expected = {"0001) good"};
         Assert.assertArrayEquals(trash.getNewest(2), expected);
     }
 
